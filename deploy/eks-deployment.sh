51ab7972cdb08b2693e063fdeaaccdaf2b626c8b
Irshad A Buchh <ibuchh@gmail.com>
2018-09-17 22:54:48 -0500

    Update eks-deployment.sh

--- deploy/eks-deployment.sh
+++ deploy/eks-deployment.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-sed 's/COMMIT_ID_TAG/'$CI_COMMIT_ID'/g' deploy/kube-deployment-template.yml > deploy/kube-deployment.yml
+sed 's/COMMIT_ID_TAG/'$CI_COMMIT_ID'/g' kube-deployment-template.yml > deploy/kube-deployment.yml
 
 kubectl apply -f deploy/kube-deployment.yml
